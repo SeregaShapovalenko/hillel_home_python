dc29b6c4a72de6dbb88ed74491211b8f4865ba65
BoRoDa <serega.shapovalenko@gmail.com>
2025-07-19 20:25:41 +0300

    home_work4_retake_2.0

--- lesson_4/homework_4.1.py
+++ lesson_4/homework_4.1.py
@@ -1,9 +1,6 @@
 lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-lst_zero = []
-for elem in lst.copy():
-    if elem == 0:
-        lst.remove(elem)
-        lst_zero.append(0)
-lst.extend(lst_zero)
+for _ in range(lst.count(0)):
+    lst.remove(0)
+    lst.append(0)
 print(lst)
 
